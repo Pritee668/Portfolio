f79dcc9aa00d730a24f5f49bc5c1214f081455be
Pritee Gupta <priteegupta517@gmail.com>
2024-10-20 16:42:52 +0530

    git commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Home.css"; 
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,8 +10,9 @@ const Home = () => {
        <p>
        Hi, I’m Pritee Gupta, a dedicated Web Developer skilled in both front-end and back-end development, specializing in the MERN stack. I’m passionate about creating dynamic websites and applications that offer seamless user experiences. Take a look at my work and skill
        </p>
+       <Link to="/projects"  className="btn primary-btn">Explore Projects</Link>
         <a href="https://pritee-gupta-portfolio.netlify.app/projects" className="btn primary-btn">
-          Explore Projects
+          
         </a>
       </div>
     </section>
